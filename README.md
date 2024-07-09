135e3f140f7dd487bd4913a113f432840d919117
Clrain <adgsfh0408@gmail.com>
2024-07-10 07:02:48 +0900

    1.0.0

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-# Chzzk High Resolution
+# Chzzk - Adblock & High Resolution
 ## 소개
 
-치지직의 그리드 시스템을 우회하여 1080p 해상도를 활성화 해주는 방법을 제공하는 사이트  
-**[치지직 언락 사이트 바로가기](https://nikuname.github.io/Chzzk-High-Resolution/)**  
+치지직의 그리드 시스템을 우회하여 1080p 해상도를 활성화 해주는 방법을 제공하는 메크로  
+[클릭하여 설치하기](/src/main.user.js?raw=True)  
+
+## 주요 기능  
+> **1. 그리드 우회 및 1080p 고해상도 지원**  
+> **2. 광고 제거**  
+
 [![방문자 수](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fnikuname.github.io%2FChzzk-High-Resolution%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true)](https://hits.seeyoufarm.com)  
 
 ![](./docs/guide2.png)  
 
-### 주의 사항
- - **로그인 불가능**
+
+## 설치  
+1. **Tampermonkey 설치**  
+   아직 설치하지 않았다면 Tampermonkey 브라우저 확장을 설치해야 합니다. 다양한 브라우저에서 찾을 수 있습니다.  
+
+   - [Tampermonkey for Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+   - [Tampermonkey for Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+   - [Tampermonkey for Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd)
+   - [Tampermonkey for Opera/OperaGX](https://addons.opera.com/en-gb/extensions/details/tampermonkey-beta/)
+
+2. **스크립트 설치**  
+    [클릭하여 설치하기](/src/main.user.js?raw=True)
+
+3. **스크립트 활성화**  
+    Tampermonkey 대시보드에서 스크립트 이름 옆에 있는 스위치를 클릭하여 스크립트를 활성화하세요.  
+
+
+### Special Thanks
+ - [hui1601 (github)](https://github.com/hui1601/chzzk-bypass-p2p)  
+
+## License  
+This project is licensed under the [MIT License](LICENSE).  
---
+++ src/main.user.js
@@ -0,0 +1,106 @@
+// ==UserScript==
+// @name         치지직 - 그리드 우회 & 광고 제거
+// @namespace    http://tampermonkey.net/
+// @description  치지직 그리드를 우회하여 1080p를 언락하고, 광고를 스킵해주는 메크로 (기여자 : hui1601)
+// @author       Nikuname, Clrain
+// @version      1.0.0
+// @match        https://chzzk.naver.com/live/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chzzk.naver.com
+// @updateURL    https://github.com/nikuname/Chzzk-Adblock-High-Resolution/raw/main/src/main.user.js
+// @downloadURL  https://github.com/nikuname/Chzzk-Adblock-High-Resolution/raw/main/src/main.user.js
+// @grant        none
+// ==/UserScript==
+
+(function(){
+    const nativeXMLHttpRequest = window.XMLHttpRequest;
+    XMLHttpRequest = function(){
+        this.eventlisteners = {};
+        const xhr = new nativeXMLHttpRequest();
+        this.xhr = xhr;
+        let keys = ['readyState', 'response', 'responseText', 'responseType', 'responseURL', 'responseXML', 'status', 'statusText', 'timeout', 'upload', 'withCredentials'];
+        for(let key of keys){
+            Object.defineProperty(this, key, {
+                get: function(){
+                    const apiRegex = /https:\/\/api.chzzk.naver.com\/service\/v3\/channels\/[a-z0-9]+\/live-detail/;
+                    const responseURL = xhr.responseURL;
+                    if(key === 'responseText' && xhr.responseType == '' && responseURL && apiRegex.test(responseURL)){
+                        try{
+                            let res = JSON.parse(xhr.response);
+                            res.content.p2pQuality = [];
+                            return JSON.stringify(res);
+                        } catch(e){
+                            console.error(e);
+                        }
+                    }
+                    if(key === 'responseText' && xhr.responseType == '' && responseURL)
+                    {
+                        let res = JSON.parse(xhr.response);
+                        if(res.ads) {
+                            res.ads = [];
+                            return JSON.stringify(res);
+                        }
+                    }
+
+                    return xhr[key];
+                },
+                set: function(value){
+                    xhr[key] = value;
+                }
+            });
+        }
+        function eventHandlerWrapper(t, eventType){
+            return function(e) {
+                if(t.eventlisteners[eventType]){
+                    for(let listener of t.eventlisteners[eventType]){
+                        if(!listener)
+                            continue;
+                        try{
+                            listener.apply(t, e);
+                        }catch(e){}
+                    }
+                }
+            };
+        }
+        let eventTypes = ['load', 'error', 'progress', 'abort', 'loadend', 'loadstart', 'timeout', 'readystatechange'];
+        for(let eventType of eventTypes){
+            xhr.addEventListener(eventType, eventHandlerWrapper(this, eventType));
+        }
+        return this;
+    };
+    XMLHttpRequest.prototype.addEventListener = function(type, listener){
+        if(!(type in this.eventlisteners)){
+            this.eventlisteners[type] = [];
+        }
+        this.eventlisteners[type].push(listener);
+    };
+    XMLHttpRequest.prototype.open = function(method, url, async){
+        this.url = url;
+        this.xhr.open(method, url, async);
+    };
+    XMLHttpRequest.prototype.send = function(){
+        this.xhr.send();
+    };
+    XMLHttpRequest.prototype.setRequestHeader = function(header, value){
+        this.xhr.setRequestHeader(header, value);
+    };
+    XMLHttpRequest.prototype.getAllResponseHeaders = function(){
+        return this.xhr.getAllResponseHeaders();
+    };
+    XMLHttpRequest.prototype.getResponseHeader = function(header){
+        return this.xhr.getResponseHeader(header);
+    };
+    XMLHttpRequest.prototype.abort = function(){
+        this.xhr.abort();
+    };
+    XMLHttpRequest.prototype.overrideMimeType = function(mime){
+        this.xhr.overrideMimeType(mime);
+    };
+    let properties = ['onload', 'onerror', 'onprogress', 'onabort', 'onloadend', 'onloadstart', 'ontimeout', 'onreadystatechange'];
+    for(let property of properties){
+        Object.defineProperty(XMLHttpRequest.prototype, property, {
+            set: function(value){
+                this.addEventListener(property.slice(2), value);
+            }
+        });
+    }
+})();
